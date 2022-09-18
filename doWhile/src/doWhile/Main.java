29dfb6d39a761d80b32d692ed2a53d5357cd2606
ilknurgg <ilknurguclu96@gmail.com>
2022-09-18 18:10:47 +0300

    Homework1

---
+++ doWhile/src/doWhile/Main.java
@@ -0,0 +1,17 @@
+package doWhile;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		int j=100;
+		do {
+			System.out.println(j);
+			j+=2;
+		}
+		while(j<10);
+		System.out.println("Do-While döngüsü bitti");
+
+	}
+
+}
---
+++ loopDemoWhile/src/loopDemoWhile/Main.java
@@ -0,0 +1,26 @@
+package loopDemoWhile;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    //For loop
+		for(int i=2; i<10; i+=2)
+		{
+			
+			System.out.println(i);
+		}
+       System.out.println("For Döngüsü bitti");
+       
+       int i=2;
+       //While loop
+       while(i<10) {
+    	   System.out.println(i);
+    	   i+=2;
+       }
+       
+       System.out.println("While Döngüsü bitti");
+	}
+
+}
+
+
